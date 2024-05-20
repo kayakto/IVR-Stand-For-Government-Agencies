e386175ab7f67bee5c570d39ac0f10f7e3946dec
kayakto <alexeev.egor.list.2004@gmail.com>
2024-05-20 23:20:22 +0500

    add cors

--- src/main/java/ru/example/IVRStandApiApplication.java
+++ src/main/java/ru/example/IVRStandApiApplication.java
@@ -39,8 +39,9 @@ public class IVRStandApiApplication {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:8080", "http://localhost:5173",
-                                "http://localhost:3000");
+                        .allowedOrigins("*")
+                        .allowedMethods("*") // добавьте нужные методы
+                        .allowedHeaders("*");
             }
         };
     }
